a8bfdde96cf65e0af93aa4829675f14f98670045
kaceyvolmar/CMPT-120L-910-20F <70246357+kaceyvolmar@users.noreply.github.com>
2020-11-03 15:33:33 -0500

    Assignment 6 & 7

--- Assignments/Assignment 6/leap_year.py
+++ Assignments/Assignment 6/leap_year.py
@@ -7,11 +7,24 @@ def leap_year(year):
     - Take in a parameter called year and return “Is a leap year” or “Not a leap year”
     """
     # Write your code here. 
+    
+	if year % 4 == 0 and year % 100 != 0:
+		print(year, "is a leap year.")
+	elif year % 100 != 0:
+		print(year, "is not a leap year.")
+	elif year % 400 == 0:
+		print(year, "is a leap year.")
+		return year
+	else: 
+		print(year, "is not a leap year.")
 
 if __name__ == "__main__":
     years = [2000, 1994, 1912, 3002, 1700, 1400]
     answers = []
     for year in years:
         answers.append(leap_year(year))
-    
-    print(answers)
+ 
+ 
+ 
+ 
+  #  print(answers)
--- Assignments/Assignment 7/prime_or_composite.py
+++ Assignments/Assignment 7/prime_or_composite.py
@@ -6,7 +6,18 @@ def prime_or_composite(number):
     
     - Take in a parameter called number and return “Prime” or “Composite”
     """
+    if number > 1:
+        for i in range (2, number):
+            if number % i == 0:
+			    print(number, "= Composite")
+			    break
+        else:
+            print(number, "= Prime")
+    else:
+        print(number, "= Composite")
     pass
+    
+    
 
 if __name__ == "__main__":
     numbers = [1, 2, 10, 31, 47, 89, 101, 103, 97, 187, 981, 19201]
@@ -16,4 +27,10 @@ if __name__ == "__main__":
     for number in numbers:
         answers.append(prime_or_composite(number))
     
-    print(answers)
+ 
+ 
+ 
+ 
+ 
+ 
+ #    print(answers)
