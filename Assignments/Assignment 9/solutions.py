8b9b02085408976bdacb15251e3a60c3780a9177
kaceyvolmar/CMPT-120L-910-20F <70246357+kaceyvolmar@users.noreply.github.com>
2020-11-17 19:17:13 -0500

    Assignment 9, Lab 8

---
+++ Assignments/Assignment 9/solutions.py
@@ -0,0 +1,10 @@
+from Calculator import Calculator
+
+calc = Calculator()
+
+print(calc.addition(3, 4))
+print(calc.subtraction(7, 2))
+print(calc.multiplication(2, 3))
+print(calc.division(6, 2))
+print(calc.square_root(16))
+print(calc.negate(10))
--- SemesterProject/app.py
+++ SemesterProject/app.py
@@ -3,10 +3,15 @@ from flask import render_template
 
 app = Flask(__name__)
 
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+Hello = Hello
+
 @app.route('/hello')
 def hello_world():
     return 'Hello Me'
+    
