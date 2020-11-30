771cc3a3b5298cd6554b26bf2ce7480460190f5d
kaceyvolmar/CMPT-120L-910-20F <70246357+kaceyvolmar@users.noreply.github.com>
2020-11-29 19:13:36 -0500

    pro changes

--- SemesterProject/finpro.py
+++ SemesterProject/finpro.py
@@ -1,6 +1,5 @@
 import logging
-from flask import Flask 
-from flask import render_template
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -9,7 +8,32 @@ app.logger.setLevel(logging.INFO)
 
 @app.route('/')
 def index():
-    app.logger.info("This page isn't finished yet.")
+    app.logger.info("Switching to Index Page.")
     return render_template('index.html')
 
 
+@app.route('/cody')
+def cody():
+    app.logger.info("Switching to Cody Page.")
+    return render_template('cody.html')
+
+@app.route('/noel')
+def noel():
+    app.logger.info("Switching to Noel Page.")
+    return render_template('noel.html')
+
+
+@app.route('/content')
+def content():
+    app.logger.info("Switching to Content Page.")
+    return render_template('content.html')
+
+@app.route('/404')
+def four_oh_four():
+    app.logger.info("Switching to 404 Page.")
+    return render_template('four_oh_four.html')
+
+@app.errorhandler(404)
+def page_not_found(error):
+    app.logger.warning("Switching to 404 Page.")
+    return render_template('four_oh_four.html'), 404
